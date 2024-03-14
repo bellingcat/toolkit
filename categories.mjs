646c2bd5a5453b56961b675357be53062340f662
Lilia Kai <liliakai@gmail.com>
2024-03-14 04:31:03 +0100

    Rendering categories
    Parse json and render category page markdown

---
+++ categories.mjs
@@ -0,0 +1,73 @@
+import fs from 'fs';
+import path from 'path';
+import pkg from './paths.mjs'
+const {getPaths, processMarkdownFile} = pkg;
+
+function getTools(pathname) {
+  return fs.readdirSync(pathname).flatMap((filename) => {
+    if (filename[0] === ".") { return null; } // ignore hidden files
+
+    // process README.md in each tool directory
+    const filepath = path.join(pathname, filename, 'README.md');
+    if (fs.existsSync(filepath)) {
+      return processMarkdownFile(filepath, filename, [], 'more/all-tools');
+    }
+
+    throw new Error(`No README.md found in ${filename}`);
+
+  });
+}
+
+const tools = getTools('gitbook/tools');
+const whitelist = ['twitter']; // only twitter for now
+
+getPaths('gitbook/categories').filter((category) => {
+  const tag = category.slug.slice(-1)[0];
+  return category.filename !== 'README.md' && whitelist.includes(tag);
+}).forEach((category) => {
+  const categoryTools = tools.filter((tool) => {
+    return tool.tags && tool.tags.includes(category.slug.slice(-1)[0]);
+  }).map((tool) => {
+    return {
+      ...tool,
+      rel: renderRelativeLink(category, tool)
+    }
+  });
+  console.log('writing', category.filepath);
+  const content = renderCategory(category, categoryTools);
+  fs.writeFileSync(category.filepath, content);
+});
+
+function renderCategory(category, categoryTools = []) {
+  return renderTitle(category) + renderTable(categoryTools);
+}
+function renderTitle(category) {
+  return `# ${category.title}\n\n`;
+}
+
+function renderCost(cost) {
+  if (!cost) {
+    return '';
+  }
+  if (cost.match(/partially free/i)) {
+    return '<mark style="background-color:orange;">Partially Free</mark>';
+  } else if (cost.match(/free/i)) {
+    return '<mark style="background-color:green;">Free</mark>';
+  } else if (cost.match(/paid/i)) {
+    return '<mark style="background-color:red;">Paid</mark>';
+  } else {
+    return cost;
+  }
+}
+
+function renderRelativeLink(category, tool) {
+  return path.relative(path.dirname(category.filepath), tool.filepath);
+}
+
+function renderTable(data) {
+  return (
+    "| Name | Description | Cost | Details |\n| --- | --- | --- | --- |\n" + data.map((row) => { 
+      return `| [**${row.title}**](${row.url}) | ${row.description} | ${renderCost(row.cost)} | [Details](${row.rel}) |`
+    }).join("\n")
+  );
+}
---
+++ paths.mjs
@@ -0,0 +1,71 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+function parseTags(str) {
+  if (!str) { return []; }
+  return str.split(',').map((tag) => tag.trim());
+}
+
+function markdownToJson(filepath) {
+  if (fs.existsSync(filepath)) {
+    const markdown = fs.readFileSync(filepath, 'utf-8');
+    const matches = markdown.match(/```(json)?\n([\s\S]+)\n```/);
+    if (matches) {
+      return JSON.parse(matches[2]);
+    }
+  }
+  return {};
+}
+
+function processMarkdownFile(filepath, filename, slug = [], webRoot) {
+  const page = filename.replace('.md', '');
+  const markdownWithMeta = fs.readFileSync(filepath, 'utf-8');
+  const { data, content } = matter(markdownWithMeta)
+  const h1 = content.match(/# (.*)/);
+  const title = (h1 && h1[1]) || page.replaceAll('-', ' ');
+
+  // try to get the cost from the content
+  const cost = (content.match(/\[x\] Partially Free/) && 'Partially Free') || (content.match(/\[x\] Free/) && 'Free') || (content.match(/\[x\] Paid/) && 'Paid') || null;
+
+  // get JSON data from JSON.md if it exists
+  const jsonFilePath = filepath.replace('README.md', 'JSON.md');
+  const json = markdownToJson(jsonFilePath);
+
+  if (page !== 'README') {
+    slug = [...slug, page];
+  }
+  return {
+    ...data,
+    slug, title, content, filepath, filename, cost,
+    href: `/${path.join(webRoot, slug.join('/'))}`,
+    ...json // json overrides content
+  };
+}
+
+function getPaths(pathname, slug = [], markdownRoot = 'gitbook', webRoot = '') {
+  const files = fs.readdirSync(pathname);
+
+  const paths = files.flatMap((filename) => {
+    if (filename[0] === ".") { return null; } // ignore hidden files
+
+    const filepath = path.join(pathname, filename);
+    if (filepath === `${markdownRoot}/README.md`) {
+      return null; // Special case for the root README
+    }
+
+    if (path.extname(filename) == ".md") {
+      if (filename === 'SUMMARY.md') {
+        return null;
+      }
+      // markdown file
+      return processMarkdownFile(filepath, filename, slug, webRoot);
+    } else {
+      // directory
+      return [ ...getPaths(filepath, [...slug, filename], markdownRoot, webRoot)];
+    }
+  });
+  return paths.filter(post => { return post && post });
+}
+
+export default {parseTags, getPaths, processMarkdownFile };
