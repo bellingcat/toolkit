d18ec6b4d0922d875e041db06c9cef5fe46c5554
Lilia Kai <liliakai@gmail.com>
2024-07-10 17:40:35 +0200

    Fix script to check for tool updates

--- src/last-updated.mjs
+++ src/last-updated.mjs
@@ -7,17 +7,22 @@ const { getTools } = tools;
 const exec = util.promisify(child_process.exec);
 
 // find out when each tool page was last updated
-const needUpdate = getTools().filter((tool) => inSummary(tool)).filter((tool) => !isUpdated(tool));
-if (needUpdate.length === 0) {
-  console.log('All tools are up to date');
-  process.exit(0);
-} else {
-  console.log(needUpdate.length, 'tools have not been updated in 1 month');
-  needUpdate.forEach((tool) => {
-    console.log(tool.title);
-  });
-  process.exit(1);
-}
+const publishedTools = getTools().filter((tool) => inSummary(tool));
+Promise.all(publishedTools.map(function(tool) {
+  return isUpdated(tool).then(function(result) { tool.isUpdated = result; });
+})).then(function() {
+  const needUpdate = publishedTools.filter((tool) => !tool.isUpdated);
+  if (needUpdate.length === 0) {
+    console.log('All tools are up to date');
+    process.exit(0);
+  } else {
+    console.log(needUpdate.length, 'tools have not been updated in 1 month');
+    needUpdate.forEach((tool) => {
+      console.log(tool.title);
+    });
+    process.exit(1);
+  }
+});
 
 async function isUpdated(tool) {
   // search git history for commits starting with GITBOOK-tool-slug-{number}: 
@@ -28,10 +33,8 @@ async function isUpdated(tool) {
   }
   const commitDate = stdout.trim();
   if (!commitDate) {
-    console.error(tool.title, 'not updated in the last month');
     return false;
   }
-  console.log(commitDate, tool.title);
   return true;
 }
 
