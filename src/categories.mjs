3cb9b58e0c49003f953b48c2b19592bbcf53373c
Lilia Kai <liliakai@gmail.com>
2024-07-22 16:51:18 +0200

    Rewrite getCategories
    Todo: update tools.mjs getCategories, pending resolution of a dependency
    loading issue

--- src/categories.mjs
+++ src/categories.mjs
@@ -1,9 +1,9 @@
 import fs from 'fs';
 import path from 'path';
 import tools from './tools.mjs'
-const { getTools, getCategories } = tools;
+const { getTools } = tools;
 import pkg from './paths.mjs'
-const {writeIfChanged, getSummary, processMarkdownFile} = pkg;
+const { writeIfChanged, getSummary, processMarkdownFile} = pkg;
 
 const allTools = getTools().filter((tool) => !tool.draft );
 const mostUsed = processMarkdownFile('gitbook/most-used.md', 'most-used', []);
@@ -11,9 +11,42 @@ const mostUsedTools = allTools.filter((tool) => (tool.tags || []).includes('most
 const mostUsedFilePath = 'gitbook/most-used.md';
 writeIfChanged(renderCategory(mostUsed, mostUsedTools), mostUsedFilePath);
 
+function getPaths(pathname) {
+  const files = fs.readdirSync(pathname);
+
+  const paths = files.flatMap((filename) => {
+    if (filename[0] === ".") { return null; }         // ignore hidden files
+    if (filename === 'SUMMARY.md') { return null; }   // ignore summaries
+
+    const filepath = path.join(pathname, filename);
+    if (path.extname(filename) == ".md") {
+      return processMarkdownFile(filepath, filename); // markdown file
+    } else {
+      return [ ...getPaths(filepath)]; // directory
+    }
+  });
+  return paths.filter(post => { return post && post });
+}
+function getCategories() {
+  const root = 'gitbook/categories';
+  return getPaths(root).filter((markdownFile) => {
+    // Take all README.md files as categories
+    return markdownFile.filename == 'README.md';
+  }).map(function(markdownFile) {
+    const tag = path.basename(markdownFile.directory);
+
+    return {
+      title: markdownFile.title,
+      content: markdownFile.content,
+      filepath: path.join(markdownFile.directory, tag + '.md'),
+      tag: tag
+    }
+  });
+}
+
 getCategories().forEach((category) => {
   const categoryTools = allTools.filter((tool) => {
-    return tool.tags && tool.tags.includes(category.slug.slice(-1)[0]);
+    return tool.tags && tool.tags.includes(category.tag);
   }).map((tool) => {
     return {
       ...tool,
@@ -25,16 +58,13 @@ getCategories().forEach((category) => {
 });
 
 function renderCategory(category, categoryTools = []) {
-  return renderTitle(category) + renderTable(categoryTools, category);
+  return renderIntro(category) + renderTable(categoryTools, category);
 }
 function renderTitle(category) {
   return `# ${category.title}\n\n`;
 }
 function renderIntro(category) {
-  if (fs.existsSync(category.introFilePath)) {
-    return fs.readFileSync(category.introFilePath, 'utf-8');
-  }
-  return '';
+  return category.content;
 }
 
 function renderCost(cost) {
@@ -72,4 +102,8 @@ function renderTable(tools, category) {
       return `| [**${row.title}**](${row.url}) | ${row.description} | ${renderCost(row.cost)} | ${renderRelativeLink(category, row)} |`
     }).join("\n")
   );
+}
+
+export default {
+  getCategories
 }
