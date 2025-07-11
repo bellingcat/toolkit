4d94caa2b1c35704b5c150bb348a7dd38b442443
Lilia Kai <liliakai@gmail.com>
2025-07-11 11:59:36 +0200

    Add category selection to add-tool workflow
    Allow none category and tag tool with category

--- src/add-tool.mjs
+++ src/add-tool.mjs
@@ -3,12 +3,17 @@ const {createTool} = pkg;
 
 // Create a new tool directory from the command line
 const toolName = process.argv[2]
+const category = process.argv[4];
 if (!toolName) {
   console.log('Usage: node add-tool.mjs "Tool Name"');
   process.exit(1);
 }
+let tags = [];
+if (category !== 'none') {
+  tags.push(category);
+}
 const slug = createTool({
   draft: true,
   name: toolName,
-  tags: []
+  tags: tags
 });
