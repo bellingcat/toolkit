b60a9c163cec0582a31b90a85d03ab2ef50230c2
Lilia Kai <liliakai@gmail.com>
2025-07-25 18:10:34 +0200

    Add workflow/script for project sync action

---
+++ src/ghproject.mjs
@@ -0,0 +1,30 @@
+import fs from 'fs'
+import path from 'path'
+import pkg from './paths.mjs'
+const {getTools, getSummary} = pkg;
+import pkg2 from './tools.mjs'
+const {findSpace} = pkg2
+
+const items = JSON.parse(fs.readFileSync('project_items.json', 'utf-8')).data.items.nodes;
+const tools = getTools();
+const summary = getSummary('gitbook');
+
+tools.forEach(function(tool) {
+  if (summary.match(path.relative('gitbook/', tool.filepath))) {
+    return; // Already published
+  }
+  console.log(tool.title);
+
+  const space = findSpace(tool.title);
+  if (!space) {
+    console.log("No space for tool", tool.title);
+    return;
+  }
+  const item = items.find((item) => item.title === tool.title);
+  if (!item) {
+    console.log("No gh project item for tool", tool.title);
+    return;
+  }
+  console.log(item);
+
+});
