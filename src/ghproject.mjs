f5d4d45793d7fce6d48ead842e7c2838d2396744
Lilia Kai <liliakai@gmail.com>
2025-08-20 04:34:26 +0200

    Fix matching project items to tools

--- src/ghproject.mjs
+++ src/ghproject.mjs
@@ -9,6 +9,9 @@ const items = JSON.parse(fs.readFileSync('project_items.json', 'utf-8')).data.no
 const tools = getTools();
 const summary = getSummary('gitbook');
 
+function itemTitle(item) {
+  return item.fieldValues.nodes.find((node) => node.field.name === "Title").text ;
+}
 tools.forEach(function(tool) {
   if (summary.match(path.relative('gitbook/', tool.filepath))) {
     return; // Already published
@@ -20,7 +23,7 @@ tools.forEach(function(tool) {
     console.error("No space for tool", tool.title);
     return;
   }
-  const item = items.find((item) => item.title === tool.title);
+  const item = items.find((item) => itemTitle(item) === tool.title);
   if (!item) {
     console.error("No gh project item for tool", tool.title);
     return;
