0cb5052c232fcbbe65eec2db2b772296440fb0f1
Lilia Kai <liliakai@gmail.com>
2025-09-17 13:26:51 -1000

    Only emit items needing updates

--- src/ghproject.mjs
+++ src/ghproject.mjs
@@ -5,50 +5,60 @@ const {getTools, getSummary} = pkg;
 import pkg2 from './tools.mjs'
 const {fetchChangeRequests, findSpace} = pkg2
 
-// Emits all project items in json with status info derived from the repo
-
-const items = JSON.parse(fs.readFileSync('project_items.json', 'utf-8'));
+// Emits all changed project items in json with status info the repo and space
 const tools = getTools();
 const summary = getSummary('gitbook');
 
 function getField(field, item) {
-  return item.fieldValues.nodes.find((node) => node.field.name === field);
-}
-function itemTitle(item) {
-  return getField("Title", item).text;
-}
-function itemStatus(item) {
-  return getField("Status", item).name;
+  return item.fieldValues.nodes.find((node) => node.field.name === field) || {};
 }
 function formatDate(dateString) {
   var date = new Date(dateString);
   return date.toISOString().replace(/T.*/,'');
 }
-tools.forEach(async function(tool) {
+const items = JSON.parse(fs.readFileSync('project_items.json', 'utf-8')).map(function(item) {
+  return {
+    id: item.id,
+    title: getField("Title", item).text,
+    status: getField("Status", item).name,
+    date_submitted: getField("Date submitted", item).date,
+    url: getField("URL", item).text,
+  }
+});
 
+tools.forEach(async function(tool) {
+  let changed = false;
   const space = findSpace(tool.title);
   if (!space) {
     console.error("No space for tool", tool.title);
     return;
   }
-  const item = items.find((item) => itemTitle(item) === tool.title);
+  const item = items.find((item) => item.title === tool.title);
   if (!item) {
     console.error("No gh project item for tool", tool.title);
     return;
   }
 
-  const json = { id: item.id, status: itemStatus(item), title: tool.title };
   if (summary.match(path.relative('gitbook/', tool.filepath))) {
-    json.status = "Published";
+    if (item.status !== "Published") {
+      item.status = "Published";
+      changed = true;
+    }
   }
 
   const changeRequests = await fetchChangeRequests(space);
   if (changeRequests.count) {
     const request = changeRequests.items[0];
-    json.date_submitted = formatDate(request.updatedAt);
-    json.url = request.urls.app;
-    json.status = "Review Requested";
+    const date = formatDate(request.updatedAt);
+    if (item.date_submitted !== date) {
+      item.date_submitted = date
+      item.url = request.urls.app;
+      item.status = "Review Requested";
+      changed = true;
+    }
   }
-  console.log(JSON.stringify(json));
+  if (changed) {
+    console.log(JSON.stringify(item));
+  }
 
 });
