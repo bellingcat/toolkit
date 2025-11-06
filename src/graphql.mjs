cad97df38575ccc86d80460ab61fbd35f0293bfc
Lilia Kai <liliakai@gmail.com>
2025-11-06 12:46:44 +0100

    Fix args to graphql functions

--- src/graphql.mjs
+++ src/graphql.mjs
@@ -1,24 +1,24 @@
 function mutation(changes) {
   return `mutation ( $project: ID!) { ${changes.join(' ')} }`;
 }
-function setField(itemId, fieldId, value) {
+function setField(itemId, fieldId, name, value) {
   return (`
-  set_field: updateProjectV2ItemFieldValue(input: {
+  set_field_${fieldId}: updateProjectV2ItemFieldValue(input: {
     projectId: $project
-    itemId: ${itemId}
-    fieldId: ${fieldId}
-    value: ${JSON.stringify(value)}
+    itemId: "${itemId}"
+    fieldId: "${fieldId}"
+    value: { ${name}: ${JSON.stringify(value)} }
   }) { projectV2Item { id } }
   `).replace(/\n/g, ' ');
 }
 function setTextField(itemId, fieldId, value) {
-  return setField(itemId, fieldId, {text: value});
+  return setField(itemId, fieldId, 'text', value);
 }
 function setSelectField(itemId, fieldId, value) {
-  return setField(itemId, fieldId, {singleSelectOptionId: value});
+  return setField(itemId, fieldId, 'singleSelectOptionId', value);
 }
 function setDateField(itemId, fieldId, value) {
-  return setField(itemId, fieldId, {date: value});
+  return setField(itemId, fieldId, 'date', value);
 }
 export default {
   mutation,
